6591c3928afa965ebb3eba6723b268b4aed862b0
Mason Hunt <masonhunt130ii@gmail.com>
2021-06-11 13:42:57 +1000

    Stable
    Except AC is in issue testing.

--- kubejs/server_scripts/recipes/__unification__/copper.js
+++ kubejs/server_scripts/recipes/__unification__/copper.js
@@ -2,8 +2,7 @@
 
 onEvent('recipes', event => {
     [   // Copper Ingot
-        'better_diving:copper_ingot'        ,
-        'caves_and_cliffs_mod:copper_ingot'
+        'better_diving:copper_ingot'
     ].forEach(ingot => {
         event.replaceOutput({} , ingot , 'create:copper_ingot');
         event.replaceInput ({} , ingot , 'create:copper_ingot');
@@ -12,7 +11,7 @@ onEvent('recipes', event => {
     [   // Copper Block
         'better_diving:copper_block'
     ].forEach(block => {
-        event.replaceOutput({} , block , 'caves_and_cliffs_mod:copper_block');
-        event.replaceInput ({} , block , 'caves_and_cliffs_mod:copper_block');
+        event.replaceOutput({} , block , 'create:copper_block');
+        event.replaceInput ({} , block , 'create:copper_block');
     });
 });
--- kubejs/startup_scripts/worldgen/caves_and_cliffs_mod/copper_ore.js
+++
@@ -1,8 +0,0 @@
-// priority: 0
-
-onEvent('worldgen.remove', event => {
-	event.removeOres(ores => {
-		// Remove Caves and Cliffs Mod stone-based copper ore (but keep deepslate variant).
-		ores.blocks = ['caves_and_cliffs_mod:copper_ore'];
-	});
-});
