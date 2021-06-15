b317264a0300fe023df1deda7f2b70a403d7c9a9
Mason Hunt <masonhunt130ii@gmail.com>
2021-06-15 10:47:53 +1000

    Removed NEC

--- kubejs/server_scripts/tags/entities/wthit/waila_blacklist.js
+++ kubejs/server_scripts/tags/entities/wthit/waila_blacklist.js
@@ -2,16 +2,5 @@
 
 onEvent('entity_type.tags', event => {
     // Blacklisting entities from being visible in WTHIT.
-	[
-		'immersive_portals:border_portal'            ,	// Immersive Portals
-		'immersive_portals:breakable_mirror'         ,
-		'immersive_portals:end_floor_portal'         ,
-		'immersive_portals:end_portal'               ,
-		'immersive_portals:general_breakable_portal' ,
-		'immersive_portals:global_tracked_portal'    ,
-		'immersive_portals:loading_indicator'        ,
-		'immersive_portals:mirror'                   ,
-		'immersive_portals:nether_portal_new'        ,
-		'immersive_portals:portal'
-	].forEach(entity => event.add('waila:blacklist', entity));
+	// [].forEach(entity => event.add('waila:blacklist', entity));
 });
